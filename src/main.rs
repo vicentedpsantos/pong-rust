fcb76a803dba971213fbe3e0755e131b0b3791ea
Vicente <vicentedpsantos@gmail.com>
2021-03-23 10:42:02 -0300

    Draw game window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 use tetra::graphics::{self, Color};
 use tetra::{Context, ContextBuilder, State};
 
-fn main() {}
+struct GameState {}
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        Ok(())
+    }
+}
+
+fn main() -> tetra::Result {
+    ContextBuilder::new("PongRust", 640, 480)
+        .quit_on_escape(true)
+        .build()?
+        .run(|_| Ok(GameState {}))
+}
